1629d8e37529f6d83bcff90aa24ff2b6c1fcd676
Guillermo Pages <public.guillermo@gmail.com>
2019-07-31 12:40:57 +0200

    feat: add mysql with dotenv support

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
+import 'dotenv/config';
 import express from 'express';
-//const { importSchema } = require('graphql-import');
 import { ApolloServer } from 'apollo-server-express';
+import mysql from 'mysql';
+
 import schema from './schema';
 import resolvers from './resolvers';
 
 const dev = process.env.NODE_ENV === 'development';
 const port = process.env.PORT || 3030;
 const graphqlPath = process.env.GRAPHQL_PATH || '/graphql';
+
+const db = mysql.createConnection({
+  host: process.env.DB_HOST || 'localhost',
+  user: process.env.DB_USER || 'myuser',
+  password: process.env.DB_PASSWORD || 'mypassword',
+  database: process.env.DB_NAME || 'mydatabase',
+});
 
 const server = new ApolloServer({
   typeDefs: schema,
