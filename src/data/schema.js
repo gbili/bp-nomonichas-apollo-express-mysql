3146cddd4aba2cd7403552320c806d8f264b0932
Guillermo Pages <public.guillermo@gmail.com>
2019-08-03 17:39:28 +0200

    feat: allow loading different connection settings based on env

--- src/data/schema.js
+++ src/data/schema.js
@@ -1,11 +1,44 @@
-import { mysqlConnection } from './connection';
+import 'dotenv/config';
 import { readFileSync } from 'fs';
+import MysqlReq from '../utils/MysqlReq';
+import logger from '../utils/logger';
 
-let databaseDump = readFileSync(`${__dirname}/schema.sql`, 'utf-8')
+logger.log('shema.js');
+const executeSchemaOntoExistingConnection = async function () {
+  logger.log('executeSchemaOntoExistingConnection');
+  await MysqlReq.query({
+    sql: readFileSync(`${__dirname}/schema.sql`, 'utf-8'),
+  });
+};
 
-mysqlConnection.query(databaseDump, (err, result) => {
-  if (err) throw err;
-  console.log(result);
-});
+if (process.env.NODE_ENV === 'npmscript') {
+  logger.log('npmscript');
+  (async function() {
+    MysqlReq.configureConnection({multipleStatements: true});
+    await executeSchemaOntoExistingConnection();
+    MysqlReq.disconnect();
+  })();
+}
 
-mysqlConnection.end(err => { if (err) throw err});
+async function prepareDatabase() {
+  const config = {};
+  if (process.env.TEST_DB_HOST) {
+    config.host = process.env.TEST_DB_HOST;
+  }
+  if (process.env.TEST_DB_USER) {
+    config.user = process.env.TEST_DB_USER;
+  }
+  if (process.env.TEST_DB_NAME) {
+    config.database = process.env.TEST_DB_NAME;
+  }
+  if (process.env.TEST_DB_PASSWORD) {
+    config.password = process.env.TEST_DB_PASSWORD;
+  }
+  MysqlReq.configureConnection({
+    multipleStatements: true,
+    ...config
+  });
+  await executeSchemaOntoExistingConnection();
+}
+
+export default prepareDatabase;
