3257c0c79ca4a1fc01796ccdc47c9b1ce7e67f57
Guillermo Pages <public.guillermo@gmail.com>
2019-08-07 22:37:33 +0200

    feat: add visible server running log

--- src/App.js
+++ src/App.js
@@ -23,7 +23,11 @@ class App {
   static start({ port, path }) {
     // set the port that the express application will listen to
     App.getInstance().listen({ port }, () => {
+      _logger.log(`#################################################`);
+      _logger.log(`#################################################`);
       _logger.log(`Server running on http://localhost:${port}${path}`);
+      _logger.log(`#################################################`);
+      _logger.log(`#################################################`);
     });
   }
 
